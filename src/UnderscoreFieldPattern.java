e5c46819fad2010675a2accf92772047c224ddca
Kieran Groble <grobleke@GROBLEKE-W540.rose-hulman.edu>
2017-02-12 22:17:10 -0500

    initial set up, c level

---
+++ src/UnderscoreFieldPattern.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.FieldNode;
+
+import graph.ClassCell;
+import graph.Graph;
+import patterns.Pattern;
+
+public class UnderscoreFieldPattern extends Pattern {
+    private int hello_world;
+    
+    @Override
+    public Graph detect(Graph graphToSearch) {
+        Graph ret = new Graph();
+        
+        for (ClassCell c : graphToSearch.getCells()) {
+            for (FieldNode f : c.getFieldNodes()) {
+                if ((f.access & Opcodes.ACC_FINAL) == 0
+                        && f.name.contains("_")) {
+                    ret.addClass(c);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+
+}
